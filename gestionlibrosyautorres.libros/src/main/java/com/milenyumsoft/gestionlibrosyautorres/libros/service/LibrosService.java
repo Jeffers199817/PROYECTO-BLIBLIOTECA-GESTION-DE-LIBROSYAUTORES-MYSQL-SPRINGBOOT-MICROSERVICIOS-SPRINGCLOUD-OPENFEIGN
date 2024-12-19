e1dcc420a69e01f3612b51cc51afa894e8ff4150
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2024-12-18 22:14:17 -0500

    Integración de la intergaz de IlibrosService

--- gestionlibrosyautorres.libros/src/main/java/com/milenyumsoft/gestionlibrosyautorres/libros/service/LibrosService.java
+++ gestionlibrosyautorres.libros/src/main/java/com/milenyumsoft/gestionlibrosyautorres/libros/service/LibrosService.java
@@ -1,4 +1,29 @@
 package com.milenyumsoft.gestionlibrosyautorres.libros.service;
 
-public class LibrosService {
+public class LibrosService implements ILibrosService {
+
+    @Override
+    public void crearLibro() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public List<Libro> traerLibros() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Libro traerLibro(Long id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Libro actualizarLibro(Long id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+
 }

850359a38d43a8bdde36f8cf007a70d1523b2fdd
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2024-12-19 23:18:50 -0500

    Integraicón de las metodos crud

--- gestionlibrosyautorres.autores/src/main/java/com/milenyumsoft/gestionlibrosyautorres/autores/service/AutorService.java
+++ gestionlibrosyautorres.autores/src/main/java/com/milenyumsoft/gestionlibrosyautorres/autores/service/AutorService.java
@@ -1,6 +1,8 @@
 package com.milenyumsoft.gestionlibrosyautorres.autores.service;
 
 import com.milenyumsoft.gestionlibrosyautorres.autores.modelo.Autor;
+import com.milenyumsoft.gestionlibrosyautorres.autores.repository.IAutorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,25 +10,28 @@ import java.util.List;
 @Service
 public class AutorService implements IAutorService {
 
+    @Autowired
+    private IAutorRepository autorRepository;
 
     @Override
     public void crearAutor(Autor autor) {
+     autorRepository.save(autor);
 
     }
 
     @Override
     public void eliminarAutor(Long id) {
-
+        autorRepository.delete(autorRepository.findById(id).get());
     }
 
     @Override
     public List<Autor> traerAutores() {
-        return List.of();
+        return autorRepository.findAll();
     }
 
     @Override
     public Autor traerAutor(Long id) {
-        return null;
+        return autorRepository.findById(id).get();
     }
 
     @Override

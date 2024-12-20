0ec42d5577574eedfee93af0fc1dc9668f3b0241
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2024-12-19 23:13:19 -0500

    Integración del mircorservico de autores

--- gestionlibrosyautorres.autores/src/main/java/com/milenyumsoft/gestionlibrosyautorres/autores/modelo/autor.java
+++ gestionlibrosyautorres.autores/src/main/java/com/milenyumsoft/gestionlibrosyautorres/autores/modelo/autor.java
@@ -1,4 +1,27 @@
 package com.milenyumsoft.gestionlibrosyautorres.autores.modelo;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@ToString
+
 public class autor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idAutor;
+    private String nombre;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate fechaNacimiento;
+    @ElementCollection
+    private List<String> idLibros;
 }
